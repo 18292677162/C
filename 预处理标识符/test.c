fd661105b3881eb9f93561b435bcb8e698c4911a
18292677162 <2254095453@qq.com>
2018-10-06 19:36:11 +0800

    -

--- 预处理标识符/test.c
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
-#include<windows.h>
-int main()
-{
-	printf("FILE:%s\nLINE:%d\nDATE:%s\nTIME:%s\n", __FILE__, __LINE__, __DATE__, __TIME__);
-	system("pause");
-	return 0;
-}
