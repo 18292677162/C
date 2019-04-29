957525337b9cec01a1ccdda892cae545f867e25c
18292677162 <2254095453@qq.com>
2019-04-29 23:06:28 +0800

    -

---
+++ Queue/Queue/queue.cpp
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <queue>
+#include <vector>
+
+#if 1
+class MyStack {
+public:
+	/** Initialize your data structure here. */
+	MyStack() {
+
+	}
+
+	/** Push element x onto stack. */
+	void push(int x) {
+
+	}
+
+	/** Removes the element on top of the stack and returns that element. */
+	int pop() {
+
+	}
+
+	/** Get the top element. */
+	int top() {
+
+	}
+
+	/** Returns whether the stack is empty. */
+	bool empty() {
+		return empty();
+	}
+};
+
+/**
+* Your MyStack object will be instantiated and called as such:
+* MyStack* obj = new MyStack();
+* obj->push(x);
+* int param_2 = obj->pop();
+* int param_3 = obj->top();
+* bool param_4 = obj->empty();
+*/
+
+int main()
+{
+	MyStack s;
+	system("pause");
+	return 0;
+}
+#endif
