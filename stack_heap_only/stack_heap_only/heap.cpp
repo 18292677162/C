5a9321b724dc99b23a9aadae27b1ed6875754d97
18292677162 <2254095453@qq.com>
2019-05-27 23:04:32 +0800

    -

---
+++ stack_heap_only/stack_heap_only/heap.cpp
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+
+//类的构造函数私有，拷贝构造函数私有，防止拷贝构造在栈上生成对象
+//提供静态成员函数，用来完成堆对象的创建
+#if 0
+class HeapOnly
+{
+public:
+	static HeapOnly* CreateObject()
+	{
+		return new HeapOnly;
+	}
+	static void DestroyObject(HeapOnly* &p)
+	{
+		if (NULL != p) {
+			delete p;
+			p = nullptr;
+		}
+	}
+	int data;
+private:
+	HeapOnly() {}
+	~HeapOnly() {}
+
+	//C++98只声明不实现
+	//HeapOnly(const HeapOnly&);
+
+	//C++11
+	//HeapOnly(const HeapOnly&) = delete;
+};
+
+int main()
+{
+	//HeapOnly* parrHeap = new HeapOnly[10];
+	//HeapOnly* pHeap = new HeapOnly;
+	//HeapOnly heap;
+
+	HeapOnly* pHeap = HeapOnly::CreateObject();
+	if (NULL != pHeap) {
+		pHeap->data = 5;
+	}
+	return 0;
+}
+
+#endif
---
+++ stack_heap_only/stack_heap_only/stack.cpp
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+
+#if 0
+//只在栈上创建，就是不能在堆上创建
+class StackOnly
+{
+public:
+	static StackOnly CreateObject()
+	{
+		return StackOnly();
+	}
+
+	StackOnly()
+	{
+		cout << "call construct function" << endl;
+	}
+
+	~StackOnly()
+	{
+		cout << "call destruction function" << endl;
+	}
+
+private:
+	void* operator new(size_t size){}
+	void* operator new[](size_t size) {}
+	void operator delete(void* p) {}
+	void operator delete[](void* p) {}
+};
+
+
+int main()
+{
+	//StackOnly* parrStack = new StackOnly[10];
+	//StackOnly* pStack = new StackOnly;
+
+	StackOnly stack;
+	system("pause");
+	return 0;
+}
+#endif
