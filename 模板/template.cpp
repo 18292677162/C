b457987956899e6541758cb878b67441f717ea49
18292677162 <2254095453@qq.com>
2019-05-31 17:57:55 +0800

    -

---
+++ 模板/template.cpp
@@ -0,0 +1,146 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+// 函数模板实例化
+#if 0
+template<class T>
+T Add(const T& left, const T& right)
+{
+	return left + right;
+}
+
+int main()
+{
+	int a1 = 10, a2 = 20;
+	double b1 = 1.3, b2 = 2.2;
+
+	// 隐式实例化
+	cout << Add(a1, a2) << endl;
+	cout << Add(b1, b2) << endl;
+	cout << Add(double(a1), b1) << endl;
+
+	// 显示实例化
+	cout << Add<double>(a2, b2) << endl;
+	system("pause");
+	return 0;
+}
+#endif
+
+// 函数模板匹配规则
+#if 0
+// 专门处理int加法函数
+int Add(const int& left, const int& right)
+{
+	return left + right;
+}
+
+//通用加法函数1
+template<class T>
+T Add(const T& left, const T& right)
+{
+	return left + right;
+}
+
+//通用加法函数2
+template<class T1,class T2>
+T1 Add(const T1& left, const T2& right)
+{
+	return left + right;
+}
+
+int main()
+{
+	// 函数模板可以和同名的非模板函数存在，优先调用非模板函数
+
+	//优先选择非模板函数
+	cout << Add(1, 2) << endl;
+	//显式实例化
+	cout << Add<int>(1, 2) << endl;
+
+	// 模板函数可以生成更加匹配的版本
+	cout << Add(1, 2.2) << endl;
+	system("pause");
+	return 0;
+}
+#endif
+
+//模板动态顺序表(vector)
+#if 0
+template<class T>
+class Vector
+{
+public:
+	Vector(size_t capacity = 10)
+		: _pData(new T[capacity])
+		, _size(0)
+		, _capacity(capacity)
+	{}
+
+	// 析构函数：类中声明，类外定义
+	~Vector();
+
+	void PushBack(const T& data)
+	{
+		_pData[_size++] = data;
+	}
+
+	void PopBack()
+	{
+		--_size;
+	}
+
+	size_t Size()
+	{
+		return _size;
+	}
+
+	T& operator[](size_t pos)
+	{
+		assert(pos < _size&&pos <= _capacity);
+		return _pData[pos];
+	}
+
+private:
+	T* _pData;
+	size_t _size;
+	size_t _capacity;
+};
+
+// 类模板函数在类外定义时，需要加模板参数列表
+template<class T>
+Vector<T>::~Vector()
+{
+	if (nullptr != _pData){
+		delete[] _pData;
+	}
+}
+
+int main()
+{
+	Vector<int> s1;
+	s1.PushBack(1);
+	s1.PushBack(2);
+	s1.PushBack(3);
+	s1.PopBack();
+
+	Vector<char> s2;
+	s2.PushBack('A');
+	s2.PushBack('B');
+	s2.PushBack('C');
+
+	for (size_t i = 0; i < s1.Size(); ++i){
+		cout << s1[i] << " ";
+	}
+	cout << endl;
+
+	for (size_t i = 0; i < s2.Size(); ++i){
+		cout << s2[i] << " ";
+	}
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
+#endif
---
+++ 模板/template_plus.cpp
@@ -0,0 +1,197 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <assert.h>
+using namespace std;
+
+template<class T1, class T2>
+class Data
+{
+public:
+	Data() { cout << "Data<T1, T2>" << endl; }
+private:
+	T1 _d1;
+	T2 _d2;
+};
+
+//全特化
+#if 0
+template<>
+class Data<int, char>
+{
+public:
+	Data() { cout << "Data<int, char>" << endl; }
+private:
+	int _d1;
+	char _d2;
+};
+
+void Test()
+{
+	Data<int, int> d1;
+	Data<int, char> d2;
+}
+
+int main()
+{
+	Test();
+	system("pause");
+	return 0;
+}
+#endif
+
+//偏特化
+#if 0
+//部分特化
+// 将第二个参数特化为int
+template <class T1>
+class Data<T1, int>
+{
+public:
+	Data() { cout << "Data<T1, int>" << endl; }
+private:
+	T1 _d1;
+	int _d2;
+};
+
+
+//参数更进一步的限制
+//两个参数偏特化为指针类型
+template <typename T1, typename T2>
+class Data <T1*, T2*>
+{
+public:
+	Data() { cout << "Data<T1*, T2*>" << endl; }
+private:
+	T1 _d1;
+	T2 _d2;
+};
+
+//两个参数偏特化为引用类型
+template <typename T1, typename T2>
+class Data <T1&, T2&>
+{
+public:
+	Data(const T1& d1, const T2& d2)
+		: _d1(d1)
+		, _d2(d2)
+	{
+		cout << "Data<T1&, T2&>" << endl;
+	}
+private:
+	const T1 & _d1;
+	const T2 & _d2;
+};
+
+void Test()
+{
+	Data<double, int> d1; // 调用特化的int版本
+	Data<int, double> d2; // 调用基础的模板
+	Data<int*, int*> d3; // 调用特化的指针版本
+	Data<int&, int&> d4(1, 2); // 调用特化的指针版本
+}
+
+int main()
+{
+	Test();
+	system("pause");
+	return 0;
+}
+#endif
+
+// 类型萃取
+// 内置类型
+struct TrueType
+{
+	static bool Get()
+	{
+		return true;
+	}
+};
+
+// 自定义类型
+struct FalseType
+{
+	static bool Get()
+	{
+		return false;
+	}
+};
+
+template<class T>
+struct TypeTraits
+{
+	typedef FalseType IsPODType;
+};
+
+template<>
+struct TypeTraits<char>
+{
+	typedef TrueType IsPODType;
+};
+
+template<>
+struct TypeTraits<int>
+{
+	typedef TrueType IsPODType;
+};
+
+template<>
+struct TypeTraits<short>
+{
+	typedef TrueType IsPODType;
+};
+
+template<>
+struct TypeTraits<long>
+{
+	typedef TrueType IsPODType;
+};
+
+template<>
+struct TypeTraits<long long>
+{
+	typedef TrueType IsPODType;
+};
+
+template<>
+struct TypeTraits<float>
+{
+	typedef TrueType IsPODType;
+};
+
+template<>
+struct TypeTraits<double>
+{
+	typedef TrueType IsPODType;
+};
+
+template<>
+struct TypeTraits<long double>
+{
+	typedef TrueType IsPODType;
+};
+
+template<class T>
+void Copy(T* dst, const T* src, size_t size)
+{
+	if (TypeTraits<T>::IsPODType::Get()){
+		memcpy(dst, src, sizeof(T)*size);
+	}
+	else{
+		for (size_t i = 0; i < size; ++i){
+			dst[i] = src[i];
+		}
+	}
+}
+
+int main()
+{
+	int a1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+	int a2[10];
+	Copy(a2, a1, 10);
+	string s1[] = { "111", "222", "333", "444" };
+	string s2[4];
+	Copy(s2, s1, 4);
+	system("pause");
+	return 0;
+}
