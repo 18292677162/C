c2fbb69fedad2d7c73f6b1495da92b3cf5239796
18292677162 <2254095453@qq.com>
2019-06-03 00:36:22 +0800

    -

--- priority_queue/Top_K.cpp
+++ priority_queue/Top_K.cpp
@@ -8,14 +8,27 @@ using namespace std;
 
 int main()
 {
-	vector<int> nums= { 3, 2, 1, 5, 6, 6,4 };
+	vector<int> nums = { 3, 2, 3, 1, 2, 4, 5, 5, 6 };
 	int k;
 	cin >> k;
+	
+	//  找数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素
+#if 0
 	if (1 <= k && k <= nums.size()){
 		sort(nums.begin(), nums.end(), greater<int>());
-		unique(nums.begin(), nums.end());
-		cout << nums.size();
+		//vector<int>::iterator end_unique = unique(nums.begin(), nums.end());
+		//nums.erase(end_unique, nums.end());
+		return nums[k - 1];
 	}
-	system("pause");
+	else{
+		return -1;
+	}
 	return 0;
+#endif
+
+	priority_queue<int> q(nums.begin(), nums.end());
+	for (size_t i = 0; i < k - 1; i++){
+		q.pop();
+	}
+	return q.top();
 }
